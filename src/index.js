15d810fff87ed21d89c9aaea116c095c985a400e
Kpa6ctep <slavko_kiselev@ukr.net>
2020-11-05 22:47:04 +0200

    05.11

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import fetchCountries from './fetchCountries.js'
 import debounce from 'lodash.debounce';
 
 const input = document.getElementById('searchInput');
+
 input.addEventListener('input', debounce((e) => {
   fetchCountries(e.target.value)
   input.value = '';
-}, 500))
+}, 700))
 
 
 
